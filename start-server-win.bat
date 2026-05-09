03d5ee5a1aaeaa750af5c0ac2f55409ec64eb428
FlaxyDitto <johnathanoren@gmail.com>
2026-05-09 22:45:50 +0300

    Delete start-server-win.bat

--- start-server-win.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Navigate to the USB drive's directory
-cd /d "%~dp0"
-:: Tell Ollama to use the models folder on this drive
-set OLLAMA_MODELS=%CD%\models
-echo Starting Ollama Server... Keep this window open!
-.\bin\ollama.exe serve
-pause
