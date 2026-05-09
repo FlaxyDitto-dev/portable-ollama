5da3783f6469b9e293c45d00f0458ecd2fe8e567
FlaxyDitto <johnathanoren@gmail.com>
2026-05-09 22:46:48 +0300

    Delete chat-unix.sh

--- chat-unix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="$(cd "$(dirname "$0")" && pwd)"
-read -p "Enter model name (e.g., llama3.2, phi3): " model
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    "$DIR/bin/ollama-darwin" run "$model"
-else
-    "$DIR/bin/ollama-linux" run "$model"
-fi
